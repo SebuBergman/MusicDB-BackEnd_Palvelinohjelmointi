aaefdce5217222b220276b5c9e4d4f0d7af15a1c
SebuBergman <basti.bergman@gmail.com>
2022-04-14 14:49:11 +0300

    Added some tests and some css changes

--- src/main/java/hhpalvelinohjelmointi/Musicdb/web/CombinedController.java
+++ src/main/java/hhpalvelinohjelmointi/Musicdb/web/CombinedController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import hhpalvelinohjelmointi.Musicdb.domain.AlbumRepository;
 import hhpalvelinohjelmointi.Musicdb.domain.SongRepository;
@@ -24,6 +25,15 @@ public class CombinedController {
 		public String musiclist(Model model) {
 			model.addAttribute("songs", songrepository.findAll());
 			model.addAttribute("albums", albumrepository.findAll());
-			return "listmusic";
+			return "musiclist";
+		}
+		
+	//Guest Endpoints
+		//Guest show database items
+		@RequestMapping(value = "/musiclistguest", method = RequestMethod.GET)
+			public String showDatabaseGuest(Model model) {
+			model.addAttribute("songs", songrepository.findAll());
+			model.addAttribute("albums", albumrepository.findAll());
+			return "musiclistguest";
 		}
 }
--- src/test/java/hhpalvelinohjelmointi/Songdatabase/SongdatabaseApplicationTests.java
+++ src/test/java/hhpalvelinohjelmointi/Songdatabase/SongdatabaseApplicationTests.java
@@ -1,13 +1,42 @@
 package hhpalvelinohjelmointi.Songdatabase;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import hhpalvelinohjelmointi.Musicdb.web.AlbumController;
+import hhpalvelinohjelmointi.Musicdb.web.CombinedController;
+import hhpalvelinohjelmointi.Musicdb.web.SongController;
+
+//@RunWith(SpringRunner.class) // JUnit4
+@ExtendWith(SpringExtension.class)   // JUnit5 eli Jupiter
 @SpringBootTest
 class SongdatabaseApplicationTests {
 
+	@Autowired
+	private CombinedController combinedController;
+	
+	@Autowired
+	private SongController songController;
+	
+	@Autowired
+	private AlbumController albumController;
+	
 	@Test
-	void contextLoads() {
+	public void combinedLoads() {
+		assertThat(combinedController).isNotNull();
 	}
-
+	
+	@Test
+	public void songControllerLoads() {
+		assertThat(songController).isNotNull();
+	}
+	
+	@Test
+	public void albumControllerLoads() {
+		assertThat(albumController).isNotNull();
+	}
 }
